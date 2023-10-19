d05a26937fb7f9bef6a30180e6cd39bd9f85f815
gdserafini <gdinizserafini@gmail.com>
2023-10-19 18:03:06 -0300

    Classe Person atualizada -> setters e verificações com MyLib

--- src/classes/Person.java
+++ src/classes/Person.java
@@ -1,11 +1,25 @@
 package classes;
 
+import aux.MyLib;
+
 public abstract class Person{
+
     private String name;
     private int age;
 
     public int getAge() { return age; }
+
     public String getName() { return name; }
+
+    public void setAge(int newAge){ 
+        if(newAge < 0) return;
+        this.age = newAge; 
+    }
+
+    public void setName(String newName){ 
+        if(MyLib.invalidString(newName)) return;
+        this.name = newName; 
+    }
 
     public String getInfo(){
         return "\nNome: " + this.name +
