8b3ad2ab87a57db6d65093260ffab8e197dbe3f2
AnaCezanoski <135081064+AnaCezanoski@users.noreply.github.com>
2023-10-19 19:30:31 -0300

    Add files via upload

---
+++ src/classes/Witness.java
@@ -0,0 +1,31 @@
+package classes;
+
+public class Witness {
+    private String address;
+    private String contact;
+    private String report;
+
+    public String getAddress() {
+        return address;
+    }
+
+    public void setAddress(String address) {
+        this.address = address;
+    }
+
+    public String getContact() {
+        return contact;
+    }
+
+    public void setContact(String contact) {
+        this.contact = contact;
+    }
+
+    public String getReport() {
+        return report;
+    }
+
+    public void setReport(String report) {
+        this.report = report;
+    }
+}
