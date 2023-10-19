1a2f0a3b7c0aad15cebcfc5708e045df5ec4e710
AnaCezanoski <135081064+AnaCezanoski@users.noreply.github.com>
2023-10-19 19:28:27 -0300

    Add files via upload

---
+++ src/classes/Suspect.java
@@ -0,0 +1,23 @@
+package classes;
+
+public class Suspect {
+    private String descrption;
+    private String address;
+
+    public String getDescrption() {
+        return descrption;
+    }
+
+    public void setDescrption(String descrption) {
+        this.descrption = descrption;
+    }
+
+    public String getAddress() {
+        return address;
+    }
+
+    public void setAddress(String address) {
+        this.address = address;
+    }
+}
+
