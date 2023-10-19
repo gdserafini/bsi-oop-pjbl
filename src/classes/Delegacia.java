96c301886820a5853559afd446674d248a80506e
Ping-uwu <pinguschoolwork@gmail.com>
2023-10-19 19:22:41 -0300

    Craição das Classes Delegacia e Policial

---
+++ src/classes/Delegacia.java
@@ -0,0 +1,14 @@
+package classes;
+
+import java.util.ArrayList;
+
+public class Delegacia {
+    private String nome;
+    private String endereco;
+    private String contato;
+
+
+    ArrayList<Policial> policiais = new ArrayList<>();
+
+
+}
---
+++ src/classes/Policial.java
@@ -0,0 +1,48 @@
+package classes;
+
+public class Policial extends Person {
+    private String cargo;
+    private float salario;
+    private String dataDeContratacao;
+    private String nomeDelegacia;
+
+    @Override
+    public String getInfo() {
+        return super.getInfo() +
+                "\nCargo: " + this.cargo +
+                "\nSalario: " + this.salario +
+                "\nDelegacia onde atua: " + this.nomeDelegacia;
+    }
+
+    public String getCargo() {
+        return cargo;
+    }
+
+    public void setCargo(String cargo) {
+        this.cargo = cargo;
+    }
+
+    public float getSalario() {
+        return salario;
+    }
+
+    public void setSalario(float salario) {
+        this.salario = salario;
+    }
+
+    public String getDataDeContratacao() {
+        return dataDeContratacao;
+    }
+
+    public void setDataDeContratacao(String dataDeContratacao) {
+        this.dataDeContratacao = dataDeContratacao;
+    }
+
+    public String getNomeDelegacia() {
+        return nomeDelegacia;
+    }
+
+    public void setNomeDelegacia(String nomeDelegacia) {
+        this.nomeDelegacia = nomeDelegacia;
+    }
+}
