547245562fd298da79632735ccc9536955f2bf09
Ping-uwu <pinguschoolwork@gmail.com>
2023-10-19 21:22:23 -0300

    Fixed delegacia

--- src/classes/Delegacia.java
+++ src/classes/Delegacia.java
@@ -10,5 +10,35 @@ public class Delegacia {
 
     ArrayList<Policial> policiais = new ArrayList<>();
 
+    public String getNome() {
+        return nome;
+    }
 
+    public void setNome(String nome) {
+        this.nome = nome;
+    }
+
+    public String getEndereco() {
+        return endereco;
+    }
+
+    public void setEndereco(String endereco) {
+        this.endereco = endereco;
+    }
+
+    public String getContato() {
+        return contato;
+    }
+
+    public void setContato(String contato) {
+        this.contato = contato;
+    }
+
+    public ArrayList<Policial> getPoliciais() {
+        return policiais;
+    }
+
+    public void addPolicial(Policial policial){
+        policiais.add(policial);
+    }
 }
