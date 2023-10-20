7d17c48aa311064a3444ffd504f3ae8d6adb41ff
Ping-uwu <pinguschoolwork@gmail.com>
2023-10-19 21:14:50 -0300

    Classe Cadastro

---
+++ src/classes/Cadastro.java
@@ -0,0 +1,19 @@
+package classes;
+
+public class Cadastro {
+
+    public void cadastraDepartamento(){}
+
+    public void cadastraDelegacia(){}
+
+    public void cadastraPolicial(){}
+
+    public void cadastraCaso(){}
+
+    public void cadastraDestemunha(){}
+
+    public void cadastraSuspeito(){}
+
+    public void salvaCaso(){}
+
+}
